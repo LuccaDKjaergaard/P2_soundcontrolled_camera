ddcc8a280d00664a4e4051874c43ed7435772ab9
LuccaDKjaergaard <lucc0108.skole@gmail.com>
2026-05-17 18:24:23 +0200

    timer added

--- main_mega/main_mega.ino
+++ main_mega/main_mega.ino
@@ -1,18 +1,36 @@
-//include libraries?
+// AVR Timer CTC Interrupts Calculator
+// v. 8
+// http://www.arduinoslovakia.eu/application/timer-calculator
+// Microcontroller: ATmega2560
+// Created: 2026-05-15T14:48:25.870Z
 
-//const int PINS
+const int PIN_TIMER = 9;
+bool state = false;
+
+void setupTimer1() {
+  noInterrupts();
+  //Clear registers:
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 0;
+
+  //2 changes at 40kHz = 20kHz
+  //40000 Hz (16000000/((49+1)*8))
+  OCR1A = 48; //Adjusted to be closer to 40kHz output
+  TCCR1B |= (1 << WGM12); //CTC
+  TCCR1B |= (1 << CS11); //Prescaler 8
+  TIMSK1 |= (1 << OCIE1A); //Output Compare Interrupt Enable Match A 
+  interrupts();
+}
 
 void setup() {
-  //pinModes
-  //attachInterrupt
-
-  //InitTimer
+  pinMode(PIN_TIMER, OUTPUT);
+  setupTimer1();
 }
 
-void loop() {
-  
-}
+void loop() {}
 
-void InitTimer() {
-
+ISR(TIMER1_COMPA_vect) {
+  digitalWrite(PIN_TIMER, state);
+  state = state^1;
 }

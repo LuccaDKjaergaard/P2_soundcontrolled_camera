3781bcc1fc7b24818c0a0111b44f383394bf692c
LuccaDKjaergaard <lucc0108.skole@gmail.com>
2026-06-17 15:42:57 +0200

    Merge branch 'improvement_beforeExam' into issue_servoBroken

--- main_pico/ISR_functions.ino
+++ main_pico/ISR_functions.ino
@@ -12,7 +12,6 @@ void ISR_SOUND() {
 void ISR_TIMER() {
   if (soundDetected) {
     if (frontlogCnt < FRONTLOGSIZE) {
-      //frontlog[frontlogCnt] = ReadADC();
       frontlog[frontlogCnt] = analogRead(micMiddle.pin);
       frontlogCnt++;
     } else if (frontlogCnt >= FRONTLOGSIZE) {
@@ -23,7 +22,6 @@ void ISR_TIMER() {
     if(backlogCnt >= BACKLOGSIZE) {
       backlogCnt = 0;
     }
-    //backlog[backlogCnt] = ReadADC();
     backlog[backlogCnt] = analogRead(micMiddle.pin);
     backlogCnt++;
   }
